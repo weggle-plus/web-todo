936cfd11b43c2f8161aaaced1a6b410d48ba59e7
rkdcodus <rcd7275@naver.com>
2024-12-15 23:21:08 +0900

    feat: db 연결

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var dotenv = require("dotenv");
+var connection = require("./mariadb");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var todosRouter = require("./routes/todos");
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//env setup
+dotenv.config();
 
-app.use(logger('dev'));
+// db 연결 테스트
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to DB:", err);
+  } else {
+    console.log("Connected to DB");
+  }
+});
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", todosRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`server running on port ${process.env.SERVER_PORT}`);
 });
 
 module.exports = app;
