266581fc6c942d2ede5f88fd11d8dafe5e761b5c
rkdcodus <rcd7275@naver.com>
2024-12-15 23:42:38 +0900

    feat(app.js): todosRouter의 공통 경로 지정 '/todo'

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", todosRouter);
+app.use("/todo", todosRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
