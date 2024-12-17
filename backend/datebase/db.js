623e7e1efbf55ed52448f5805203576ebd60f800
Next-dreamer <mjo2640@gmail.com>
2024-12-17 12:23:54 +0900

    refactor: db.js의 DB 경로를 절대 경로로 수정 및 잘몬된 todos.db 삭제

--- backend/datebase/db.js
+++ backend/datebase/db.js
@@ -1,12 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
 
-// 데이터베이스 파일 경로 설정
-const db = new sqlite3.Database('./todos.db', (err) => {
-  if (err) {
-    console.error('Error opening database:', err.message);
-  } else {
-    console.log('Connected to SQLite3 database.');
-  }
+// 운영체제마다 파일 경로의 형식이 다른 점을 보완하기 위한 JS 내장 모듈
+const path = require('path'); 
+
+// 절대 경로로 데이터베이스 파일 경로 설정
+const dbPath = path.join(__dirname, 'todos.db');
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) return console.error('Error opening database:', err.message);
+  return console.log(`Connected to SQLite3 database. at ${dbPath}`);
 });
 
 module.exports = db;
