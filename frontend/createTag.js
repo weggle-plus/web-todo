17f735d6cd625ce6543c91bd62100dcdbdba49ef
rkdcodus <rcd7275@naver.com>
2024-12-17 20:16:27 +0900

    feat: 빈 목록 문구 태그 생성 함수 작성

--- frontend/createTag.js
+++ frontend/createTag.js
@@ -7,18 +7,18 @@ function createTodoTag(id, content) {
                     <li data-id=${id} class="contents_list" >
                         <div>
                         <input type="checkbox" class="checkBox"  />
-                        <input for="" value="${content}"  readonly  class="readonly">
+                        <input for="" value="${content}"  disabled  class="readonly">
                         </div>
                         <span class="input_btn_wrap">
-                        <span class="edit_btn_wrap">
-                            <button class="btn_gray_line update">수정</button>
-                            <button class="btn_red_line delete">삭제</button>
-                        </span>
+                          <span class="edit_btn_wrap">
+                              <button class="btn_gray_line mainBtn update">수정</button>
+                              <button class="btn_red_line mainBtn delete">삭제</button>
+                          </span>
     
-                        <span class="confirm_btn_wrap">
-                            <button class="btn_0f confirm">완료</button>
-                            <button class="btn_gray_line cancel">취소</button>
-                        </span>
+                          <span class="confirm_btn_wrap">
+                              <button class="btn_0f mainBtn confirm">완료</button>
+                              <button class="btn_gray_line mainBtn cancel">취소</button>
+                          </span>
                         </span>
                     </li>
                 `
@@ -34,11 +34,11 @@ function createDoneTag(id, content) {
                   <li data-id=${id} class="contents_list" >
                       <div>
                       <input type="checkbox" class="checkBox" checked/>
-                      <input for="" value="${content}"  readonly  class="readonly">
+                      <input for="" value="${content}"  disabled  class="readonly">
                       </div>
                       <span class="input_btn_wrap">
                       <span class="edit_btn_wrap">
-                          <button class="btn_red_line delete">삭제</button>
+                          <button class="btn_red_line mainBtn delete">삭제</button>
                       </span>
                       </span>
                   </li>
@@ -46,4 +46,17 @@ function createDoneTag(id, content) {
   );
 }
 
-export { createTodoTag, createDoneTag };
+function createNoTodoTag() {
+  const todoUl = document.getElementById("getTodoData");
+  return todoUl.insertAdjacentHTML(
+    "beforeend",
+    `<li  class="none_list">할 일 항목이 없습니다</li>`
+  );
+}
+
+function createNoDoneTag() {
+  const doneUl = document.getElementById("getDoneData");
+  return doneUl.insertAdjacentHTML("beforeend", `<li class="none_list">완료 항목이 없습니다</li>`);
+}
+
+export { createTodoTag, createDoneTag, createNoTodoTag, createNoDoneTag };
