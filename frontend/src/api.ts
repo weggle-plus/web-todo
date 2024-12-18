2d9e46b3473b0e05220bc225a7b969be115146c0
Jini <wlsdl6685@gmail.com>
2024-12-18 17:21:10 +0900

    style: 개행 정리

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 const BASE_URL = 'http://localhost:3000/todos';
 
-export async function fetchData<T>(url?: string, options?: RequestInit): Promise<T|null> {
+export async function fetchData<T>(url?: string, options?: RequestInit): Promise<T | null> {
     try {
         const response = await fetch(`${BASE_URL}/${url}`, options);
 
@@ -9,7 +9,7 @@ export async function fetchData<T>(url?: string, options?: RequestInit): Promise
         }
 
         // 응답 본문 없는 경우
-        if (response.status === 204){
+        if (response.status === 204) {
             return null;
         }
 
