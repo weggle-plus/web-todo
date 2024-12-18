f392d5e4c56805fdc76fa212da7892234b34eeda
asdfbn12345 <asdfbn12345@gmail.com>
2024-12-18 20:23:46 +0900

    Add: backend cors 라이브러리 추가

--- backend/app.ts
+++ backend/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 // import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from "cors";
 
 import indexRouter from "./routes/index";
 import tasksRouter from "./routes/tasks";
@@ -18,6 +19,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/tasks", tasksRouter);
