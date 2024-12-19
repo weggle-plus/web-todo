1242d8d3df6b3423a2e4bf380551825412dbf70d
jihye <wlgul817@gmail.com>
2024-12-19 18:28:42 +0900

    feat: todo리스트 데이터 화면에 출력

--- reactfe/src/App.js
+++ reactfe/src/App.js
@@ -1,16 +1,23 @@
 import Input from "./component/Input";
 import TodoList from "./component/TodoList";
 import apiModules from "./api";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    apiModules.getTodoItems().then(setTodoList);
+  }, []);
+
   return (
     <div className="container">
       <nav>
         <div className="nav_todo">할 일 목록</div>
       </nav>
       <section>
-        <Input addTodoItem={apiModules.addTodoItem}/>
-        <TodoList />
+        <Input addTodoItem={apiModules.addTodoItem} />
+        <TodoList todoList={todoList} />
       </section>
     </div>
   );
--- reactfe/src/component/TodoList.js
+++ reactfe/src/component/TodoList.js
@@ -1,10 +1,23 @@
-function TodoList() {
+function TodoList({ todoList }) {
   return (
     <>
       <div className="todo_wrap">
         <div>TO DO</div>
         <div id="todo_list">
-          <span id="todo_none">할 일 항목이 없습니다.</span>
+          {todoList.length === 0 ? (
+            <span id="todo_none">할 일 항목이 없습니다.</span>
+          ) : (
+            todoList.map((item) => {
+              return (
+                <div key={item.id} className="todo_item">
+                  <input type="checkbox"></input>
+                  <label className="todo_item_title">{item.title}</label>
+                  <button className="btn_modify">수정</button>
+                  <button className="btn_delete">삭제</button>
+                </div>
+              );
+            })
+          )}
         </div>
       </div>
       <div className="done_wrap">
