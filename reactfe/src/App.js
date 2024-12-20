98599a651815fcf9715f3dd28843b7dd5eacdfbf
jihye <wlgul817@gmail.com>
2024-12-20 16:31:39 +0900

    feat: DoneList 컴포넌트 구현 및 체크박스 기능 구현

--- reactfe/src/App.js
+++ reactfe/src/App.js
@@ -2,13 +2,37 @@ import Input from "./component/Input";
 import TodoList from "./component/TodoList";
 import apiModules from "./api";
 import { useEffect, useState } from "react";
+import DoneList from "./component/DoneList";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [doneList, setDoneList] = useState([]);
 
   useEffect(() => {
-    apiModules.getTodoItems().then(setTodoList);
+    getTodoItems();
   }, []);
+
+  const getTodoItems = () => {
+    apiModules.getTodoItems().then((items) => {
+      if (items !== undefined) {
+        const todoItems = items.filter((item) => item.done !== 1);
+        const doneItems = items.filter((item) => item.done === 1);
+
+        setTodoList(todoItems);
+        setDoneList(doneItems);
+      }
+    });
+  };
+
+  const toggleCheckbox = async (todoItem) => {
+    try {
+      await apiModules.checkDone(todoItem);
+
+      getTodoItems();
+    } catch (error) {
+      console.log("error : ", error);
+    }
+  };
 
   return (
     <div className="container">
@@ -17,7 +41,8 @@ function App() {
       </nav>
       <section>
         <Input addTodoItem={apiModules.addTodoItem} />
-        <TodoList todoList={todoList} />
+        <TodoList todoList={todoList} toggleCheckbox={toggleCheckbox} />
+        <DoneList doneList={doneList} toggleCheckbox={toggleCheckbox} />
       </section>
     </div>
   );
--- reactfe/src/component/DoneList.js
+++ reactfe/src/component/DoneList.js
@@ -1,21 +1,21 @@
-function TodoList({ todoList, toggleCheckbox }) {
+function DoneList({ doneList, toggleCheckbox }) {
   return (
     <>
-      <div className="todo_wrap">
-        <div>TO DO</div>
-        <div id="todo_list">
-          {todoList === undefined || todoList.length === 0 ? (
-            <span id="todo_none">할 일 항목이 없습니다.</span>
+      <div className="done_wrap">
+        <div>DONE</div>
+        <div id="done_list">
+          {doneList === undefined || doneList.length === 0 ? (
+            <span id="todo_none">완료 항목이 없습니다.</span>
           ) : (
-            todoList.map((item) => {
+            doneList.map((item) => {
               return (
                 <div key={item.id} className="todo_item">
                   <input
                     type="checkbox"
+                    checked
                     onChange={() => toggleCheckbox(item)}
                   ></input>
                   <label className="todo_item_title">{item.title}</label>
-                  <button className="btn_modify">수정</button>
                   <button className="btn_delete">삭제</button>
                 </div>
               );
@@ -27,4 +27,4 @@ function TodoList({ todoList, toggleCheckbox }) {
   );
 }
 
-export default TodoList;
+export default DoneList;
