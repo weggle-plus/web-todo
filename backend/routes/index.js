27fe1e11a1e1e90fc59043bd4a43224ae8e66c0d
Jade Kim <csia19030@gmail.com>
2024-12-21 23:50:10 +0900

    feat: todo api에 validation 추가, validator 모듈화

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,16 @@
-const {addTodo, getTodos, removeTodo, editTodo} = require('../controller/indexController');
-var express = require('express');
-var router = express.Router();
+const {addTodo, getTodos, removeTodo, editTodo} = require('../controllers/indexController');
+const {body} = require('express-validator');
+const {checkValidateError} = require('../middleware/validator');
+const express = require('express');
+const router = express.Router();
 
 router.use(express.json());
 
 //add a todo item
-router.post('/', addTodo);
+router.post('/', [
+    body('title').notEmpty().withMessage('add todo: validation error - title'),
+    checkValidateError
+], addTodo);
 
 //get all todo and done items
 router.get('/', getTodos);
@@ -14,6 +19,10 @@ router.get('/', getTodos);
 router.delete('/:id', removeTodo);
 
 //edit a single todo item
-router.put('/:id', editTodo);
+router.put('/:id', [
+    body('title').optional().notEmpty().withMessage('edit todo: validation error - title'),
+    body('status').optional().isIn([0, 1]).withMessage('edit todo: validation error - status'),
+    checkValidateError
+], editTodo);
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,14 +1,23 @@
-const {signup, login} = require('../controller/usersController');
+const {signup, login} = require('../controllers/userController');
 const express = require('express');
-const { validateSignup, validateLogin } = require('../validator/userValidator');
+const {body} = require('express-validator');
+const {checkValidateError} = require('../middleware/validator');
 const router = express.Router();
 
 router.use(express.json())
 
 //signup
-router.post('/signup', validateSignup, signup);
+router.post('/signup', [
+    body('username').notEmpty().withMessage('signup validation error - username'),
+    body('password').notEmpty().isLength({min: 6, max: 255}).withMessage('signup validation error - password'),
+    checkValidateError
+], signup);
 
 //login
-router.post('/login', validateLogin, login);
+router.post('/login', [
+    body('username').notEmpty().withMessage('login validation error - username'),
+    body('password').notEmpty().withMessage('login validation error - password'),
+    checkValidateError
+], login);
 
 module.exports = router;
