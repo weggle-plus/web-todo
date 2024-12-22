7a808a2863b60710384bfd39f9bde2fddfd13a4b
jihye <wlgul817@gmail.com>
2024-12-22 11:12:06 +0900

    refactor: 필요없는 파일 삭제

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,5 +10,3 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
-reportWebVitals();
